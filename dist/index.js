02a81d9f7e769bfa9eef59c1a28c09f2cae97b23
MariusNWK <marius.nowak@epitech.eu>
2022-10-14 15:03:42 +0200

    [ADD] pug

--- dist/index.js
+++ dist/index.js
@@ -5,11 +5,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const path_1 = __importDefault(require("path"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT;
+// view engine setup
+app.set("views", path_1.default.join(__dirname, "views"));
+app.set("view engine", "pug");
 app.get("/", (req, res) => {
-    res.send("Testing nodemon");
+    res.render("index", { title: "Express" });
 });
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
--- index.ts
+++ index.ts
@@ -1,13 +1,18 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("Testing nodemon");
+  res.render("index", { title: "Express" });
 });
 
 app.listen(port, () => {
