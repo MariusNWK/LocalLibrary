37f8777e462e1e5f69ce51775ea559e6b6b8528a
MariusNWK <marius.nowak@epitech.eu>
2022-10-14 14:52:33 +0200

    [New init] Typescript project

--- nodemon.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// test nodemon
-router.get('/test', (req, res) => {
-  res.send('Nodemon functionnal');
-});
-
-module.exports = router;
