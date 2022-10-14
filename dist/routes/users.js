a8c32b637ae40191a256d638ef6361028406aa10
MariusNWK <marius.nowak@epitech.eu>
2022-10-14 15:52:10 +0200

    [ADD] /users/cool exercise

--- dist/routes/users.js
+++ dist/routes/users.js
@@ -5,4 +5,7 @@ const router = (0, express_1.Router)();
 router.get("/", (req, res) => {
     res.render("index", { title: "Users route" });
 });
+router.get("/cool", (req, res) => {
+    res.render("body", { title: "Tutorial Exercise", desc: "You're so cool" });
+});
 exports.default = router;
--- routes/users.ts
+++ routes/users.ts
@@ -6,4 +6,8 @@ router.get("/", (req: Request, res: Response) => {
   res.render("index", { title: "Users route" });
 });
 
+router.get("/cool", (req: Request, res: Response) => {
+  res.render("body", { title: "Tutorial Exercise", desc: "You're so cool" });
+});
+
 export default router;
